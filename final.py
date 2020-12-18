959ea297cf35694b297067b9d5c9879aff883987
BLIA THAO <blia451054@mail.fresnostate.edu>
2020-12-17 18:20:49 -0800

    finished plain english

---
+++ final.py
@@ -0,0 +1,13 @@
+"""
+The program is to display the number of grades,
+the averaqge grade, and the percentage of grades
+that are above the average from the Final.txt, which 
+contains students grades on the final exam. 
+first we will write a main function to initialize the application
+and a calculate_percent_above grade function to calculate
+the percentage of grades that are above the grade average.
+we have to make a list for the final.txt.
+The given number of grades is 24.
+The given Average grade is 83.25.
+The percentage of grades above average is 54.17%. 
+"""
