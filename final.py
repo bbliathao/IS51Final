d18e95fccd14c532382351012cef4a17d1019f82
BLIA THAO <blia451054@mail.fresnostate.edu>
2020-12-17 19:28:09 -0800

    finished

--- final.py
+++ final.py
@@ -22,3 +22,23 @@ The percentage of grades above average is 54.17%.
 
 
 """
+
+def main():
+    calculate_percent_above_average()
+    
+    inflie = open ("final(1).txt", 'r')
+    grades = [line.rstrip() for line in infile]
+    infile.close()
+
+for i in range(len(grades)):
+    grades[i] = int(grades[i])
+average = sum(grades) / len(grades)
+num = 0 # number of grades above average
+for grade in grades:
+    if grade > average:
+        num += 1
+print("Number of grades:", len(grades))
+print("Average grade:", average)
+print("Percentage of grades above average: {0:.2f}%",.format(100 * num / len(grades))
+
+main
